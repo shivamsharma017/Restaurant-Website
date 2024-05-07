6503b7d1f7408d4a7a2a304507feaf5524967534
shivamsharma017 <shivamshrama889@gmail.com>
2024-05-08 03:28:44 +0530

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import Dosa from "../images/dosa.jpg";
+import Chola from "../images/chhola.jpg";
+import Idli from "../images/idli.jpg";
+import MasalaDosa from "../images/masala.jpg";
+import Paneer from "../images/paneer.jpg";
+import Gujrati from "../images/gujrati.jpeg";
+
+export const MenuList = [
+  {
+    name: "Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Dosa,
+    price: 180,
+  },
+  {
+    name: "Chola",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chola,
+    price: 281,
+  },
+  {
+    name: "Idli Sambhar",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Idli,
+    price: 250,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: MasalaDosa,
+    price: 110,
+  },
+  {
+    name: "Paneer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Paneer,
+    price: 1200,
+  },
+  {
+    name: "Gujrati",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Gujrati,
+    price: 927,
+  },
+];
